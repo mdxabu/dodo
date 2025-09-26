6462459fdc4ba40609c8f78fff97bb06992fb3e5
mdxabu <abdullahfakrudeen2020@gmail.com>
2025-09-26 15:37:13 +0530

    Initial commit for dodo

---
+++ cmd/root.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2025 @mdxabu
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+var rootCmd = &cobra.Command{
+	Use:   "dodo",
+	Short: "",
+	Long: `dodo is a Go-based command-line tool designed to make Git safer and easier for developers by solving pains...`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 @mdxabu
+
+*/
+package main
+
+import "github.com/mdxabu/dodo/cmd"
+
+func main() {
+	cmd.Execute()
+}
